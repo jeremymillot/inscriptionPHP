417270e78abd6877787cbd93a52db0528e5c8498
jeremymillot <jeremymillot@live.fr>
2021-10-13 10:14:38 +0200

    add missing ; line5 authentifierPHP

--- controller/authentifier.php
+++ controller/authentifier.php
@@ -2,7 +2,7 @@
 require('authentification.php');
 
 if (isset($_POST['submit'], $_POST['login'], $_POST['password'])) {
-    $sanitized_data = sanitizeDataAuth($_POST['login'], $_POST['password'])
+    $sanitized_data = sanitizeDataAuth($_POST['login'], $_POST['password']);
     if (verifyUser($sanitized_data['login'], $sanitized_data['password'])) {
         header('Location: ../index.php');
     } else {
